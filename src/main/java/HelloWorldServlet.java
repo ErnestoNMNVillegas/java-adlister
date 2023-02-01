e0772b2d3de8756250549ec5d9c9bfc6d66e7493
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-02-01 16:01:46 -0600

    servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,32 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int count = Integer.parseInt(req.getParameter("count"));
+        String name = req.getParameter("name");
 
+
+        try {
+            count = Integer.parseInt(req.getParameter("count"));
+        } catch (Exception e) {
+            count = 1;
+        }
+
+        if (name == null){
+            name = "World";
+        }
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, " + name + "!</h1>");
+        for (int i = 1; i < count; i++){
+        out.println("<h1>Hello, " + name + "!</h1>");
+    }
     }
 }
