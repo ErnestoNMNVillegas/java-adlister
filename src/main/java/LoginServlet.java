34daa76c26e00807cbc99ffeed16388c451fa104
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-02-06 14:54:26 -0600

    mvc exercise

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+
+        if (req.getMethod().equalsIgnoreCase("post")) {
+            if (username.equals("admin") && password.equals("password")) {
+                resp.sendRedirect("/profile.jsp");
+            }
+        }
+    }
+}
